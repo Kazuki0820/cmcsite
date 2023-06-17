a73913307000c11a9b4dc2f591b572ea66866be1
Kazuki0820 <n97655346@gmail.com>
2023-06-17 18:54:15 +0900

    These changes are to deploy Heroku!

--- apisite/urls.py
+++ apisite/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+import django_on_heroku
+import dj_database_url
 
 app_name = 'apisite'
 
--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
+import django_on_heroku
+import dj_database_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
